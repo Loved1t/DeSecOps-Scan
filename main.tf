6728820a03487bf2ccaefadddd32bbaa2a54e8af
Michael <mnlovedit@gmail.com>
2025-05-04 12:26:41 +0100

    added SAST scan and python code

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  region = "eu-west-2" # London region
-}
-resource "aws_s3_bucket" "terraform_statefile" {
-  bucket = "terraform_statefile"
-}
+  region = "eu-west-2" # London region}
+resource "aws_s3_bucket" "terraform_statefile" 
+  bucketterraform = "terraform_statefile"
