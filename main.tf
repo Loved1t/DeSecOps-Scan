dd2445e98ca0a4561417681c9153ebc6fcad15c0
Michael <mnlovedit@gmail.com>
2025-06-01 21:01:49 +0100

    Change TF stae

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 provider "aws" {
    region = "eu-west-2" # London region
  }
- resource "aws_s3_bucket" "terraform_statefile" {
-   bucket = "terraform_statefile"
+ resource "aws_s3_bucket" "terraform-statefile-michael" {
+   bucket = "terraform-statefile-michael"
  }
+ terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-statefile-michael"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
